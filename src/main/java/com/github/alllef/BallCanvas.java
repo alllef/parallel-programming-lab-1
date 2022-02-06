e503aa6ff4bffef3b6bd726f4eeca035898a556d
alekz29 <alekz29@gmail.com>
2022-02-06 15:59:03 +0200

    Add counter of balls in pocket. Finish task 2 of lab. Problem with ConcurrentModificationException.

--- src/main/java/com/github/alllef/BallCanvas.java
+++ src/main/java/com/github/alllef/BallCanvas.java
@@ -7,10 +7,17 @@ import java.util.ArrayList;
 public class BallCanvas extends JPanel {
     private ArrayList<Ball> balls = new ArrayList<>();
     Pocket pocket = new Pocket();
+    int ballsInPocket = 0;
+    JLabel jLabel = new JLabel("Number of balls in pocket = " + ballsInPocket);
+
+
+    BallCanvas() {
+        super();
+        this.add(jLabel);
+    }
 
     public void add(Ball b) {
         this.balls.add(b);
-
     }
 
     @Override
@@ -18,11 +25,12 @@ public class BallCanvas extends JPanel {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         for (Ball b : balls) {
-
             if (b.isInside(pocket)) {
                 balls.remove(b);
                 b.setInsidePocket(true);
-            } else
+                jLabel.setText("Number of balls in pocket = " + ++ballsInPocket);
+            }
+            else
                 b.draw(g2);
 
             pocket.draw(g2);
